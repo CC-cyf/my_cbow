76065ed18510cc13a5bcdd3c1ca9a44247b48161
CC-cyf <cyf1258405329@hotmail.com>
2019-09-19 17:25:16 +0800

    完善了vocab.py

--- make_vocab.py
+++
@@ -1,5 +0,0 @@
-filename="alice_in_wonderland.txt"
-with open(filename) as alice:
-	context=alice.read()
-words=context.split()
-for word in words:
--- vocab.py
+++ vocab.py
@@ -1,4 +1,22 @@
-index=0
-def append(vocab_table,word):
-	vocab['word']=index
-	index+=index
+import json
+class vocab:
+	index=0
+	vocab_table={}
+	#在词典内增加一个词
+	def append(self,word):
+		self.vocab[word] = self.index
+		self.index += self.index
+
+	#将词典保存到.json文件中
+	def save_to_file(self):
+		with open('vocab_table.json','w') as vt:
+			json.dump(self.vocab_table,vt)
+		print('The vocaburary table has been saved as "vocab_table.json"')
+
+	def __init__(self,filename):
+		with open(filename) as fileread:
+			context=fileread.read()
+		words=context.replace('*','').split()
+		for each in words:
+			if each not in words:
+				self.append(each)
