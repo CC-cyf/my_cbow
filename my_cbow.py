6b942334e87ff73c5caa1c929c939100c2c29948
Chen <34164851+MrNobodyBee@users.noreply.github.com>
2019-10-30 13:45:51 +0800

    归一化

--- my_cbow.py
+++ my_cbow.py
@@ -1,5 +1,6 @@
 from vocab import vocab
 import numpy as np
+import math
 
 vt=vocab('alice')
 learning_rate=0.5
@@ -13,22 +14,23 @@ for i in range(0,5):
         each_word=vt.article[each_word_index]
         windowed_words=vt.get_windowed_words(center_word_index=each_word_index)
         content_vector=vt.get_content_vector(center_word_index=each_word_index)
-        result=np.matmul(content_vector,vt.output_matrix)
+        result=np.dot(content_vector,vt.output_matrix)
         standard_result=np.zeros(vt.vocab_size)
         standard_result[vt.vocab_table[each_word].id]=1
-        diff_output_matrix=np.matmul(content_vector.T,(result-standard_result))
+        diff_output_matrix=np.dot(content_vector.T,(result-standard_result))
         vt.output_matrix=vt.output_matrix-learning_rate*diff_output_matrix
-        diff_content_vector=np.matmul((result-standard_result),vt.output_matrix.T)
+        diff_content_vector=np.dot((result-standard_result),vt.output_matrix.T)
         for each in windowed_words:
             vt.vocab_table[each].vector=vt.vocab_table[each].vector-learning_rate*diff_content_vector
         #print("learning...",((i+1)/5*(each_word_index+1)/vt.article_len))
+        print(each_word_index)#just for debug
     learning_rate=learning_rate-0.1
     print("loop ",i," done")
 v1=vt.vocab_table['alice'].vector
 v2=vt.output_matrix[:,vt.vocab_table['was'].id]
 np.shape(v1)
 np.shape(v2)
-res=float((np.matmul(v1,v2.T))/(np.sum(v1*v1*v2*v2)))
+res=float((np.dot(v1,v2.T))/math.sqrt((np.sum(v1*v1*v2*v2))))
 print("the similarity between 'alice' and 'was' is ",res)
 
 
